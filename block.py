1af48d3c0651c8e14e651e6cb3622c36bb1f64c6
0wlie22 <dari.sedova@gmail.com>
2022-12-31 22:14:21 +0200

    Add figure motion

--- block.py
+++ block.py
@@ -1,60 +1,142 @@
 import pygame as pg
 import numpy as np
+from dataclasses import dataclass
+from enum import Enum
+from pygame.math import Vector2
+from random import choice
+from pygame.surface import Surface
+
+
+@dataclass
+class Shape:
+    color: str
+    points: list[Vector2]
 
 
 ORANGE_RICKY = [
-    [False, False, True],
-    [True, True, True]
+    Vector2(0, 40),
+    Vector2(60, 40),
+    Vector2(60, 0),
+    Vector2(40, 0),
+    Vector2(40, 20),
+    Vector2(0, 20),
+    Vector2(0, 40),
 ]
 BLUE_RICKY = [
-    [True, False, False],
-    [True, True, True]
+    Vector2(0, 40),
+    Vector2(60, 40),
+    Vector2(60, 20),
+    Vector2(20, 20),
+    Vector2(20, 0),
+    Vector2(0, 0),
+    Vector2(0, 40),
 ]
 CLEVELAND_Z = [
-    [True, True, False],
-    [False, True, True]
+    Vector2(0, 0),
+    Vector2(0, 20),
+    Vector2(20, 20),
+    Vector2(20, 40),
+    Vector2(60, 40),
+    Vector2(60, 20),
+    Vector2(40, 20),
+    Vector2(40, 0),
+    Vector2(0, 0),
 ]
 RHODE_ISLAND_Z = [
-    [False, True, True],
-    [True, True, False]
+    Vector2(0, 40),
+    Vector2(40, 40),
+    Vector2(40, 20),
+    Vector2(60, 20),
+    Vector2(60, 0),
+    Vector2(20, 0),
+    Vector2(20, 20),
+    Vector2(0, 20),
+    Vector2(0, 40),
 ]
 HERO = [
-    [True, True, True, True]
+    Vector2(0, 0),
+    Vector2(0, 20),
+    Vector2(80, 20),
+    Vector2(80, 0),
+    Vector2(0, 0),
 ]
 TEEWEE = [
-    [False, True, False],
-    [True, True, True]
+    Vector2(0, 40),
+    Vector2(60, 40),
+    Vector2(60, 20),
+    Vector2(40, 20),
+    Vector2(40, 0),
+    Vector2(20, 0),
+    Vector2(20, 20),
+    Vector2(0, 20),
+    Vector2(0, 40),
 ]
 SMASHBOY = [
-    [True, True],
-    [True, True]
+    Vector2(0, 0),
+    Vector2(0, 40),
+    Vector2(40, 40),
+    Vector2(40, 0),
+    Vector2(0, 0),
 ]
-BLOCKS = list(map(np.array, [
-    BLUE_RICKY,
-    CLEVELAND_Z,
-    HERO,
-    ORANGE_RICKY,
-    RHODE_ISLAND_Z,
-    SMASHBOY,
-    TEEWEE,
-]))
+SHAPES = [
+    Shape("orange", ORANGE_RICKY),
+    Shape("blue", BLUE_RICKY),
+    Shape("red", CLEVELAND_Z),
+    Shape("green", RHODE_ISLAND_Z),
+    Shape("cyan", HERO),
+    Shape("purple", TEEWEE),
+    Shape("yellow", SMASHBOY),
+]
 
 
-class Block:
-    def __init__(self, shape: list[np.ndarray], size: tuple[int, int] = (20, 20)):
-        self.shape: list[np.ndarray] = shape
-        self.size: tuple[int, int] = size
+class Direction(Enum):
+    RIGHT = 1
+    LEFT = 2
 
-    def draw(self, display):
-        # Iterate over shape and draw rectangles
-        for row in range(len(self.shape)):
-            for column in range(len(self.shape[row])):
-                if self.shape[row][column]:
-                    rect = pg.Rect((20 + 20 * column, 20 + 20 * row), (20, 20))
-                    pg.draw.rect(display, "orange", rect)
 
-    def handle_event(self, event):
+class Block(pg.sprite.Sprite):
+    def __init__(self, shape: Shape):
+        super().__init__()
+        self.shape: Shape = shape
+        self.step: int = 20
+        self.speed = 2
+        self.pos: Vector2 = Vector2(20, 20)
+        self.surface: Surface = Surface((80, 80), pg.SRCALPHA, 32).convert_alpha()
+        self.rect = pg.draw.polygon(self.surface, self.shape.color, self.shape.points)
+
+    def draw(self, display: Surface):
+        display.blit(self.surface, self.pos, self.rect)
+
+    def fall(self):
+        self.pos.y += self.speed
+
+    def handle_event(self, event) -> None:
         if event.type == pg.KEYDOWN:
             if event.key == pg.K_UP:
-                self.shape = np.rot90(self.shape, 1)
-                
+                self.__rotate()
+            if event.key == pg.K_RIGHT:
+                self.__move(Direction.RIGHT)
+            if event.key == pg.K_LEFT:
+                self.__move(Direction.LEFT)
+            if event.key == pg.K_SPACE:
+                self.__inc_speed()
+        if event.type == pg.KEYUP:
+            if event.key == pg.K_SPACE:
+                self.__dec_speed()
+
+    def __rotate(self) -> None:
+        self.surface = pg.transform.rotate(self.surface, 90)
+        self.rect = self.surface.get_rect()
+
+    def __move(self, direction: Direction) -> None:
+        match direction:
+            case Direction.RIGHT:
+                self.pos.x += self.step
+            case Direction.LEFT:
+                self.pos.x -= self.step
+
+    def __inc_speed(self):
+        self.speed *= 3
+
+    def __dec_speed(self):
+        self.speed /= 3
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from random import choice
 import pygame as pg
-from block import Block, BLOCKS
+from block import Block, SHAPES
 
 
 class Window:
@@ -9,10 +9,12 @@ class Window:
         self.clock = pg.time.Clock()
         self.fps = fps
         self.run = True
-        self.blocks = []
+        self.blocks: pg.sprite.Group = pg.sprite.Group()
+        self.current_block: Block = None
 
     def start(self):
-        self.blocks.append(Block(shape=choice(BLOCKS)))
+        self.blocks.add(self.new_block())
+        self.current_block = self.blocks.sprites()[-1]
 
         while self.run:
             for event in pg.event.get():
@@ -21,20 +23,28 @@ class Window:
                 if event.type == pg.KEYDOWN:
                     if event.key == pg.K_ESCAPE:
                         self.run = False
-                for block in self.blocks:
-                    block.handle_event(event)
+                    if event.key == pg.K_n:
+                        self.blocks.add(self.new_block())
+                        self.current_block = self.blocks.sprites()[-1]
+                self.current_block.handle_event(event)
 
             self.display.fill("black")
 
+            self.current_block.fall()
             for block in self.blocks:
                 block.draw(self.display)
 
             pg.display.flip()
             self.clock.tick(self.fps)
 
+    @staticmethod
+    def new_block() -> Block:
+        return Block(shape=choice(SHAPES))
+
 
 def main():
-    window = Window((700, 720))
+    height, width = 720, 700
+    window = Window((width, height))
     window.start()
 
 
