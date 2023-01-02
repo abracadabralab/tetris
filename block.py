3d1a656b742ad23c651e4ce78835f4257fffb8b3
0wlie22 <dari.sedova@gmail.com>
2023-01-03 01:08:10 +0200

    Add shape motion in array grid

--- block.py
+++ block.py
@@ -1,8 +1,16 @@
+<<<<<<< Updated upstream
 import pygame as pg
 import numpy as np
+=======
+from enum import Enum
+from random import choice
+>>>>>>> Stashed changes
 
+import numpy as np
+import pygame as pg
 
 ORANGE_RICKY = [
+<<<<<<< Updated upstream
     [False, False, True],
     [True, True, True]
 ]
@@ -17,17 +25,59 @@ CLEVELAND_Z = [
 RHODE_ISLAND_Z = [
     [False, True, True],
     [True, True, False]
+=======
+    [None, None, True],
+    [True, True, True]
+]
+BLUE_RICKY = [
+    [True, None, False],
+    [True, True, True]
+]
+CLEVELAND_Z = [
+    [True, True, None],
+    [None, True, True]
+]
+RHODE_ISLAND_Z = [
+    [None, True, True],
+    [True, True, None]
+>>>>>>> Stashed changes
 ]
 HERO = [
     [True, True, True, True]
 ]
 TEEWEE = [
+<<<<<<< Updated upstream
     [False, True, False],
+=======
+    [None, True, None],
+>>>>>>> Stashed changes
     [True, True, True]
 ]
 SMASHBOY = [
     [True, True],
     [True, True]
+<<<<<<< Updated upstream
+=======
+]
+SHAPES = list(map(np.array, [
+    BLUE_RICKY,
+    CLEVELAND_Z,
+    HERO,
+    ORANGE_RICKY,
+    RHODE_ISLAND_Z,
+    SMASHBOY,
+    TEEWEE,
+]))
+
+COLORS = [
+    "orange",
+    "blue",
+    "red",
+    "green",
+    "cyan",
+    "purple",
+    "yellow",
+>>>>>>> Stashed changes
 ]
 BLOCKS = list(map(np.array, [
     BLUE_RICKY,
@@ -53,8 +103,52 @@ class Block:
                     rect = pg.Rect((20 + 20 * column, 20 + 20 * row), (20, 20))
                     pg.draw.rect(display, "orange", rect)
 
+<<<<<<< Updated upstream
+=======
+class Block:
+    def __init__(self, shape: list[np.ndarray], grid: list[list], screen_size: tuple[int, int]):
+        self.grid: list[list] = grid
+        self.shape: list[np.ndarray] = shape
+        self.block_size: tuple[int, int] = (20, 20)
+        self.step: int = 20
+        self.screen_size: tuple[int, int] = screen_size
+
+        self.start_x: int = int(self.screen_size[0] / (self.step * 2))
+        self.start_y: int = 0
+        self.end_x: int = self.start_x + len(self.shape[0]) - 1
+        self.end_y: int = self.start_y + len(self.shape) - 1
+
+        self.shift: int = 0
+        print(self.shape, self.end_y, self.start_y - 1, self.end_x, self.start_x - 1)
+
+    def add_to_grid(self):
+        for i in range(len(self.shape)):
+            for j in range(len(self.shape[i])):
+                self.grid[i][j + self.start_x] = self.shape[i][j]
+
+    def fall(self, level):
+        for row in range(self.end_y, self.start_y - 1, -1):
+            for block in range(self.end_x, self.start_x - 1, -1):
+                self.grid[row + 1 + level][block] = self.grid[row + level][block]
+                self.grid[row + level][block] = None
+
+    def __move(self, direction: Direction):
+        match direction:
+            case Direction.RIGHT:
+                self.shift += 1
+            case Direction.LEFT:
+                self.shift -= 1
+
+>>>>>>> Stashed changes
     def handle_event(self, event):
         if event.type == pg.KEYDOWN:
             if event.key == pg.K_UP:
                 self.shape = np.rot90(self.shape, 1)
+<<<<<<< Updated upstream
                 
+=======
+            if event.key == pg.K_RIGHT:
+                self.__move(Direction.RIGHT)
+            if event.key == pg.K_LEFT:
+                self.__move(Direction.LEFT)
+>>>>>>> Stashed changes
--- main.py
+++ main.py
@@ -4,15 +4,31 @@ from block import Block, BLOCKS
 
 
 class Window:
-    def __init__(self, size: tuple, fps: int = 60):
+    def __init__(self, size: tuple, fps: int = 5):
+        self.screen_size: tuple[int, int] = size
         self.display = pg.display.set_mode(size)
         self.clock = pg.time.Clock()
+<<<<<<< Updated upstream
         self.fps = fps
         self.run = True
         self.blocks = []
 
     def start(self):
         self.blocks.append(Block(shape=choice(BLOCKS)))
+=======
+        self.fps: int = fps
+        self.run: bool = True
+        self.blocks: list = []
+        self.current_block: Block = None
+        self.grid: list[list] = [[None] * int(size[0] / 20) for _ in range(int(size[1] / 20))]
+        self.step: int = 20
+        self.block_level: int = 0
+
+    def start(self):
+        self.blocks.append(Block(choice(SHAPES), self.grid, self.screen_size))
+        self.current_block = self.blocks[-1]
+        self.current_block.add_to_grid()
+>>>>>>> Stashed changes
 
         while self.run:
             for event in pg.event.get():
@@ -21,6 +37,7 @@ class Window:
                 if event.type == pg.KEYDOWN:
                     if event.key == pg.K_ESCAPE:
                         self.run = False
+<<<<<<< Updated upstream
                 for block in self.blocks:
                     block.handle_event(event)
 
@@ -28,13 +45,43 @@ class Window:
 
             for block in self.blocks:
                 block.draw(self.display)
+=======
+                self.current_block.handle_event(event)
+
+            self.display.fill("black")
+
+            if self.block_level <= (self.screen_size[1]/self.step - 3):
+                self.current_block.fall(self.block_level)
+                self.block_level += 1
+            else:
+                self.block_level = 0
+                self.blocks.append(Block(choice(SHAPES), self.grid, self.screen_size))
+                self.current_block = self.blocks[-1]
+                self.current_block.add_to_grid()
+
+            self.draw_grid()
+>>>>>>> Stashed changes
 
             pg.display.flip()
             self.clock.tick(self.fps)
 
+<<<<<<< Updated upstream
 
 def main():
     window = Window((700, 720))
+=======
+    def draw_grid(self):
+        for i in range(len(self.grid)):
+            for j in range(len(self.grid[i])):
+                if self.grid[i][j]:
+                    rect = pg.Rect((self.step * j, self.step * i), (self.step, self.step))
+                    pg.draw.rect(self.display, "purple", rect)
+
+
+def main():
+    width, height = 700, 720
+    window = Window((width, height))
+>>>>>>> Stashed changes
     window.start()
 
 
